f7e2aeb5c8dba4e926a50de7fb482d0e366b314a
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-07-30 15:30:55 -0400

    Updated schemas

--- models/review.js
+++ models/review.js
@@ -3,8 +3,9 @@ const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
     review: String,
-    userID: {type: Schema.Types.ObjectId,ref: "User"},
-    businessID: {type: Schema.Types.ObjectId,ref: "Business"}
+    rating: Number,
+    userID: String,
+    businessID: String
 });
 
 const Review = mongoose.model("Exercise", reviewSchema);
