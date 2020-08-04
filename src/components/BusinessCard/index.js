a5ced77a60e15a9812a4f9cf7aced7083d9fff16
Kuro sammy <sam.hc.yoo@gmail.com>
2020-08-03 20:16:37 -0400

    basic html templated done on htmlmaster (#6)
    * created landingpage folder and files along with new services card in indexHtml
    
    * finished landing page and added style to services card
    
    * created star rating card and added more styling
    
    * finshed browsing page
    
    * created pages
    
    * navbar and footer added
    
    * browse page
    
    Co-authored-by: Achilles <63617922+Chet1317@users.noreply.github.com>

---
+++ src/components/BusinessCard/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./style.css";
+
+function Businesscard() {
+  return (
+    <div className="footerContainer">
+    <div className="footer">
+        <footer>
+            <div className="container-fluid">
+                <div className="row justify-content-center">
+                    <div className="col-md-3">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href="#">Claim your Business</a></li>
+                            <li><a href="#">Advertise with us</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-md-3">
+                        <h3>About</h3>
+                        <ul>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Term of Service</a></li>
+                        </ul>
+                    </div>
+
+                    <div className="col-lg-3 item social"><a href="#"><i className="fab fa-facebook"></i></a><a href="#"><i className="fab fa-twitter"></i></a><a href="#"><i className="fab fa-snapchat"></i></a><a href="#"><i className="fab fa-instagram"></i></a>
+                        <p className="copyright">Spartans © 2020</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+    );
+}
+
+export default Footer;
