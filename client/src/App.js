e4839553f234f8a262c95c38f0a697447bc6ed17
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-08-08 10:03:02 -0400

    Update server.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/NavBar.js";
 import Footer from "./components/Footer.js";
 import Modal from "./components/Modal.js";
 import Temp from "./pages/Temp.js";
-import Business from "./pages/Newbusinesspage.js";
+import Business from "./pages/NewBusinessPage.js";
 import Userdash from "./pages/Userdash.js";
 import AboutUs from "./pages/AboutUs.js";
 import SignUpPage from "./pages/SignUpPage.js";
@@ -22,10 +22,10 @@ function App() {
         <Route exact path="/userdashboard/:id" component={Userdash} />
 
         <Route exact path="/business/:id" component={Temp} />
-        <Route exact path="/newbusiness/" component={Business} />
-        <Route exact path="/signuppage/" component={SignUpPage} />  
-        <Route exact path="/aboutus/" component={AboutUs} />  
-        <Route exact path="/termscondition/" component={TermsCondition} />          
+        <Route exact path="/newbusiness" component={Business} />
+        <Route exact path="/signuppage" component={SignUpPage} />  
+        <Route exact path="/aboutus" component={AboutUs} />  
+        <Route exact path="/termscondition" component={TermsCondition} />          
 
         <Footer />
         <Modal />
--- client/src/pages/Newbusinesspage.js
+++ client/src/pages/Newbusinesspage.js
@@ -1,5 +1,6 @@
 import React, {useRef} from "react";
 import axios from 'axios';
+import './NewBusinessPage.css'
 
 function Newbusinesspage() {
   const category = useRef()
